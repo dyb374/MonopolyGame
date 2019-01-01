555d548e053edddfa48c7ab8ba381221cc88a8ed
Andrew <ybdong374@126.com>
2019-01-01 22:38:41 +0800

    需求变更2

--- src/main/java/Cup.java
+++ src/main/java/Cup.java
@@ -7,13 +7,15 @@ public class Cup {
 
     private Dice dice1 = new Dice();
     private Dice dice2 = new Dice();
+    private Dice dice3 = new Dice();
 
     public void roll(){
         dice1.roll();
         dice2.roll();
+        dice3.roll();
     }
 
     public Integer getTotal(){
-        return dice1.getFaceValue() + dice2.getFaceValue();
+        return dice1.getFaceValue() + dice2.getFaceValue() + dice3.getFaceValue();
     }
 }
